26eb3953d0df5f7f31299d4e05eca5841b39f60c
adrielle12 <125271374+adrielle12@users.noreply.github.com>
2023-05-09 11:54:07 -0300

    aaaa

---
+++ BookCardSQL.sql
@@ -0,0 +1,44 @@
+use BDBookCard
+go
+Insert LIVRO ('LIVAUTOR') values ('Dom casmurro','Machado de assis')
+Insert LIVRO ('LIVAUTOR') values ('Vida secas', 'Glaciliano Ramos')
+--select * from livro
+Insert CAPITULO (CAPDESCRICAO, LIVID) values ('Fuga',1)
+Insert CAPITULO (CAPDESCRICAO, LIVID) values ('Festa',2)
+Insert CAPITULO (CAPDESCRICAO, LIVID) values ('Sinha vitoria',3)
+Insert CAPITULO (CAPDESCRICAO, LIVID) values ('Baleia',4)
+Insert CAPITULO (CAPDESCRICAO, LIVID) values ('Mudança',5)
+
+--select * from capitulo
+go
+insert LISTACARD (LISNOME,LISOVERVIEW,CAPID) values ('Triteza')
+insert LISTACARD (LISNOME,LISOVERVIEW,CAPID) values ('Desfortuna')
+
+  INSERT VALUES ('Na planície avermelhada os juazeiros alargavam duas manchas verdes.
+ Os infelizes tinham caminhado o dia inteiro, estavam cansados e
+ famintos. Ordinariamente andavam pouco, mas como haviam repousado
+ bastante na areia do rio seco, a viagem progredira bem três léguas.
+ Fazia horas que procuravam uma sombra. A folhagem dos juazeiros apareceu
+ longe, através dos galhos pelados da catinga rala',1)
+
+ --select * from capitulo
+go
+
+ INSERT CAPITULO (CAPDESCRICAO, LIVID) values ('Fabiano'6)
+ INSERT CAPITULO (CAPDESCRICAO, LIVID) values ('Educação'7)
+ INSERT CAPITULO (CAPDESCRICAO, LIVID) values ('Seca' 8)
+ INSERT CAPITULO (CAPDESCRICAO, LIVID) values ('Perdição'9)
+ INSERT CAPITULO (CAPDESCRICAO, LIVID) values ('Mudanças'10)
+
+ go
+
+
+ INSERT VALUES ('Estavam à procura de uma novilha perdida, Fabiano e Baleia, quando um dos filhos se aproximou e perguntou algo.
+ Fabiano se incomodava com perguntas,
+ não tinha o direito de saber nem o dever de responder. 
+ Falaria com Sinhá Vitória para tratar da educação das crianças, 
+ ela não tinha tempo para isso, cuidando da casa, mas os garotos atormentavam demais Fabiano.' 2)
+
+
+
+
